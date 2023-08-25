12c552bde3a6496f285cc13dc50f9e45da70b29b
Gabriel Bastarz <100977023+gabebastarz1@users.noreply.github.com>
2023-08-25 13:51:44 -0300

    Add files via upload

---
+++ Linguagem de Programação - JAVA/Lista 1/Ex5.java
@@ -0,0 +1,16 @@
+/*5 – Crie um arquivo chamado TrianguloComBarraN.java. Implemente um programa que exiba na
+saída padrão um triângulo de asteriscos semelhante ao mostrado a seguir. Utilize a sequência de
+escape \n para desenvolver esse programa.
+*
+**
+***
+****
+*****
+ */
+
+public class Ex5 {
+  public static void main(String[] args){
+    System.out.println("*\n**\n***\n****\n*****");
+  }
+}
+//Gabriel Bastarz Martins Ramos
---
+++ Linguagem de Programação - JAVA/Lista 1/Ex6.java
@@ -0,0 +1,13 @@
+/*6 – Crie um arquivo chamado Cursos.java. Implemente um programa que exiba na saída padrão a
+sigla e o nome de alguns cursos da Fatec de acordo com o padrão abaixo. Utilize a sequência de
+escape \t para desenvolver esse programa.
+F01
+Lógica de Programação
+F02
+Linguagem de Programação
+F03
+Programação Orientada a Objeto */
+public class Ex6 {
+  
+}
+//Gabriel Bastarz Martins Ramos
