8a541d2274010c2917b359ac279762811795518e
Gabriel Bastarz <100977023+gabebastarz1@users.noreply.github.com>
2023-08-27 11:47:58 -0300

    Update Ex08.java

--- Linguagem de Programação - JAVA/Lista 1 - Introdução/Ex08.java
+++ Linguagem de Programação - JAVA/Lista 1 - Introdução/Ex08.java
@@ -1,6 +1,6 @@
 /*8 - Crie um arquivo chamado FrasePreferida.java. Implemente um programa que mostre na saída
 padrão a sua frase preferida. Compile e execute esse programa. */
-public class Ex8 {
+public class Ex08 {
   public static void main(String[] args){
     System.out.println("Viva sua vida como se cada ato seu fosse se tornar uma lei universal.");
   }
