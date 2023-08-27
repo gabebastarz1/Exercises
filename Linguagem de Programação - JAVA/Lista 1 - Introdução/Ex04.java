3cf625f41b57e4c2034ab9c020acbdebffa44f38
Gabriel Bastarz <100977023+gabebastarz1@users.noreply.github.com>
2023-08-27 11:47:22 -0300

    Update Ex04.java

--- Linguagem de Programação - JAVA/Lista 1 - Introdução/Ex04.java
+++ Linguagem de Programação - JAVA/Lista 1 - Introdução/Ex04.java
@@ -7,7 +7,7 @@ desenvolver esse programa.
 ****
 *****
 */
-public class Ex4 {
+public class Ex04 {
   public static void main(String[] args){
     System.out.println("*");
     System.out.println("**");
