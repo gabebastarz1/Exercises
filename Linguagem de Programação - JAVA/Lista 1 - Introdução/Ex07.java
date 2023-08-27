d42913f2fe42caeb4a4b80222d804007727406b1
Gabriel Bastarz <100977023+gabebastarz1@users.noreply.github.com>
2023-08-27 11:47:50 -0300

    Update Ex07.java

--- Linguagem de Programação - JAVA/Lista 1 - Introdução/Ex07.java
+++ Linguagem de Programação - JAVA/Lista 1 - Introdução/Ex07.java
@@ -2,7 +2,7 @@
 seguinte saída. Compile e execute esse programa.
 Hello World 1
 Hello World 2 */
-public class Ex7 {
+public class Ex07 {
   public static void main(String[] args){
     System.out.println("Hello World 1");
     System.out.println("Hello World 2");
